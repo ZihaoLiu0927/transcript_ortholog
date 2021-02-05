a601c2a6a3cac4a1d995c5fabf0bf957e34b0179
ZihaoLiu0927 <66043240+ZihaoLiu0927@users.noreply.github.com>
2021-02-05 17:25:57 -0500

    Update EA_histgram_visualize.py

--- ClusterResult_StatAndVisual/EA_histgram_visualize.py
+++ ClusterResult_StatAndVisual/EA_histgram_visualize.py
@@ -16,16 +16,17 @@ def getoptions():
     parser.add_argument("-ie", "--input_e", dest="input_e", required=True, help="Input exon region file name")
     parser.add_argument("-if", "--input_f", dest="input_f", required=True, help="Input fragment file name")
     parser.add_argument("-p", "--prefix", dest="prefix", required=True, help="a prefix for output file names")
+    parser.add_argument("-f", "--filter", dest="filter", default=None, help="a gene list file that will be used to subset the input data")
     parser.add_argument("-G", "--columnG", dest="columnG", default='gene_id', help="specify the column name for unite feature")
     parser.add_argument("-T", "--columnT", dest="columnT", default='transcript_id', help="specify the column name for feature of which numbers will be counted")
     parser.add_argument("-F", "--columnF", dest="columnF", default='fragment_id', help="specify the column name for feature of which numbers will be counted")
     parser.add_argument("-E", "--columnE", dest="columnE", default='fusion_id', help="specify the column name for feature of which numbers will be counted")
     parser.add_argument("-R", "--columnR", dest="columnR", default='annotation_frequency', help="specify the column name for feature type assigned by EA tool to calculate variable feature ratio, could be one of: Constitutive, Common and Unique")
     parser.add_argument("-o", "--output", dest="output", required=True, help="Output path")
-    parser.add_argument("-s", "--separFeature", dest="separFeature", action='store_false', help="output a separate histgram figure for each feature")
-    parser.add_argument("-l", "--lengthhist", dest="lengthhist", action='store_false', help="output a distribution histgram figure for length of each feature")
-    parser.add_argument("-r", "--ratiohist", dest="ratiohist", action='store_false', help="output a distribution histgram figure for length of each feature")
-    parser.add_argument("-v", "--varFeatureNumber", dest="varFeatureNumber", action='store_false', help="output a distribution histgram figure for variable ratio of each feature")
+    parser.add_argument("-s", "--separFeature", dest="separFeature", action='store_true', help="output a separate histgram figure for each feature")
+    parser.add_argument("-l", "--lengthhist", dest="lengthhist", action='store_true', help="output a distribution histgram figure for length of each feature")
+    parser.add_argument("-r", "--ratiohist", dest="ratiohist", action='store_true', help="output a distribution histgram figure for length of each feature")
+    parser.add_argument("-v", "--varFeatureNumber", dest="varFeatureNumber", action='store_true', help="output a distribution histgram figure for variable ratio of each feature")
     args = parser.parse_args()
     return(args)
 
@@ -124,8 +125,8 @@ def calculate_varFeatureRatio(varList, allList):
     return df_merge
 
 def hist_plot(dat, bins, colors, text=True, omitText = []):
-    xs = []
-    ys = []
+    x_coords = []
+    y_coords = []
     first_height = len(dat[dat.values < bins[1]])
     #second_height = len(dat[(dat.values >= bins[1]) & (dat.values < bins[2])])
     #ylim = max(first_height, second_height) * 1.1
@@ -143,11 +144,11 @@ def hist_plot(dat, bins, colors, text=True, omitText = []):
         plt.vlines(x, 0, y, color = col, linewidth=44/shrink)
         if text and (omitIter not in omitText):
             plt.text(x, y, y, fontsize = 10/shrink, horizontalalignment='center',verticalalignment='bottom')
-        xs.append(x)
-        ys.append(y)
+        x_coords.append(x)
+        y_coords.append(y)
         omitIter+=1
-    plt.ylim(0, max(ys)*1.1)
-    return xs, ys
+    plt.ylim(0, max(y_coords)*1.1)
+    return x_coords, y_coords
 
 
 
@@ -159,8 +160,8 @@ def visualize_feature(ind, out, prefix, title, fname, kbins=10, xlabelrotation=F
         bins = generate_bins(ind, kbins+1, 99.9, ceil = binCeil, hardcut = hardcut)
     fig = plt.figure(figsize=(10,6), facecolor='white',edgecolor='black')
     ax = fig.add_subplot(111)
-    xs, ys = hist_plot(ind, bins, ['steelblue'] * (len(bins)-1))
-    tick_pos = [1] + xs
+    x_coords, y_coords = hist_plot(ind, bins, ['steelblue'] * (len(bins)-1))
+    tick_pos = [1] + x_coords
     tick_pos = [i+0.5 for i in tick_pos]
     ax.set_xticks(tick_pos)
     if not hardcut:
@@ -188,8 +189,8 @@ def counts_projectToColor(lowCol, highCol, counts):
 
 def heatmap_ratio_visualize(data, ax, heat_nrows, tick_pos, lowCol=(0, 0, 0), highCol=(255, 0, 0), kb = 10):
     heat_bins = generate_bins([0,1], nb = heat_nrows, merge_tail=100, ceil=False)
-    heat_xs = heat_bins[0:heat_nrows]
-    heat_xs = [e+0.05 for e in heat_xs]
+    heat_x_coords = heat_bins[0:heat_nrows]
+    heat_x_coords = [e+0.05 for e in heat_x_coords]
     heat_starts = tick_pos[0:(kb+1)]
     heat_ends = tick_pos[1:(kb+2)]
     res = []
@@ -200,7 +201,7 @@ def heatmap_ratio_visualize(data, ax, heat_nrows, tick_pos, lowCol=(0, 0, 0), hi
             each = ind[(ind.values < heat_bins[i+1]) & (ind.values >= heat_bins[i])]
             counts.append(each.count())
         heat_colors = counts_projectToColor(lowCol, highCol, counts)
-        ax.hlines(heat_xs, heat_starts[b], heat_ends[b], color = heat_colors, linewidth = 65/heat_nrows)
+        ax.hlines(heat_x_coords, heat_starts[b], heat_ends[b], color = heat_colors, linewidth = 65/heat_nrows)
         res.append(counts)
     return res
 
@@ -214,22 +215,24 @@ def visualize_combined(list_main, list2, list3, df2_ratio, df3_ratio, out, prefi
     ylabel2 = 'Exon regions \n per gene'
     ylabel3 = 'Fragment \n per gene'
     ylabel4 = 'Ratio of variable \n exon region'
-    ylabel5 = 'Ratio of variable \n fragment'
+    ylabel5 = 'Ratio of \nvariable fragment'
     titile = 'Distribution of number of transcripts per gene'
     lowColor = (0, 255, 0)
     highColor = (255, 0, 0)
 
     font2 = {'family' : 'Times New Roman',
     'weight' : 'normal',
-    'size'   : 15,
+    'size'   : 13,
     }
     font3 = {'family' : 'Times New Roman',
     'weight' : 'normal',
     'size'   : 10,
     }
-    
-    bins = generate_bins(dat[dat>=4] , kbins-2, hardcut = True) # kbins (including a merged top 5% bin) 
-    bins = [1, 2, 3] + bins
+    if max(dat) > 100:
+        lastbin = max(dat)
+    else:
+        lastbin = 101
+    bins = [1, 2, 3, 4, 18, 32, 46, 59, 73, 87, 100, lastbin]
 
     ticklinewidth = 0.5
     colors = sns.color_palette("hls", kbins+1)
@@ -240,8 +243,8 @@ def visualize_combined(list_main, list2, list3, df2_ratio, df3_ratio, out, prefi
     sns.set_style("whitegrid")
     fig = plt.figure(figsize=(10,10), facecolor='white',edgecolor='black')
     grid = plt.GridSpec(7,1 , wspace=0, hspace=0.15)
-    ys = []
-    xs = []
+    y_coords = []
+    x_coords = []
     ###############################
     # plot section 1
     ax1 = fig.add_subplot(grid[0,0])
@@ -250,16 +253,26 @@ def visualize_combined(list_main, list2, list3, df2_ratio, df3_ratio, out, prefi
     ax1.spines['bottom'].set_visible(False)
     ax1.set_title(titile, fontsize=14)
 
-    xs, ys = hist_plot(dat, bins, colors, text=False)
-    
-    break_y_up = np.percentile(ys, 100/kbins*(kbins-1)) * 0.7
-    break_y_down = np.percentile(ys, 100/(kbins)*(kbins-2)) * 1.4
-    ax1.set_ylim(break_y_up, max(ys) * 1.2)
-    for i in range(0, len(bins)-1):
-        if ys[i] >= break_y_up:
-            ax1.text(xs[i], ys[i], ys[i], fontsize = 10/shrink, horizontalalignment='center',verticalalignment='bottom')
+    x_coords, y_coords = hist_plot(dat, bins, colors, text=False)
 
-    tick_pos = [1] + xs
+    find_sec_bar = 0
+    break_y_up = np.percentile(y_coords, 100/kbins*(kbins-1)) * 0.7
+    break_y_down = np.percentile(y_coords, 100/(kbins)*(kbins-2)) * 1.4
+    if (break_y_up <= break_y_down):
+        for i in range(3, kbins):
+            break_y_down = np.percentile(y_coords, 100/(kbins)*(kbins-i)) * 1.4
+            if break_y_down < break_y_up:
+                find_sec_bar = 1
+                break
+        if not find_sec_bar:
+            print("warning: odd distribution!")
+    
+    ax1.set_ylim(break_y_up, max(y_coords) * 1.2)
+    for i in range(0, len(bins)-1):
+        if y_coords[i] >= break_y_up:
+            ax1.text(x_coords[i], y_coords[i], y_coords[i], fontsize = 10/shrink, horizontalalignment='center',verticalalignment='bottom')
+
+    tick_pos = [1] + x_coords
     tick_pos = [i+0.5 for i in tick_pos]
 
     ##################################
@@ -269,13 +282,13 @@ def visualize_combined(list_main, list2, list3, df2_ratio, df3_ratio, out, prefi
     ax2.spines['top'].set_visible(False)
     ax2.spines['bottom'].set_visible(False)
     ax2.set_ylabel(ylabel,font2)
-    #ax2.plot(xs, ys, color = 'steelblue', lw = 1) # fit a line
+    #ax2.plot(x_coords, y_coords, color = 'steelblue', lw = 1) # fit a line
     
     hist_plot(dat, bins, colors, text=False)
     ax2.set_ylim(0, break_y_down)
     for i in range(0, len(bins)-1):
-        if ys[i] <= break_y_down:
-            ax2.text(xs[i], ys[i], ys[i], fontsize = 10/shrink, horizontalalignment='center',verticalalignment='bottom')
+        if y_coords[i] <= break_y_down:
+            ax2.text(x_coords[i], y_coords[i], y_coords[i], fontsize = 10/shrink, horizontalalignment='center',verticalalignment='bottom')
 
 
     ################################
@@ -287,7 +300,9 @@ def visualize_combined(list_main, list2, list3, df2_ratio, df3_ratio, out, prefi
     for i in range(0, len(bins)-1):
         genes = dat[(dat.values < bins[i+1]) & (dat.values >= bins[i])].index.tolist()
         feature2 = dat2[genes].tolist()
+        feature2 = [x for x in feature2 if str(x) != 'nan']
         feature3 = dat3[genes].tolist()
+        feature3 = [x for x in feature3 if str(x) != 'nan']
         feature2s.append(feature2)
         feature3s.append(feature3)
         ratio2 = df2_ratio[df2_ratio['name'].isin(genes)]['ratio'].tolist()
@@ -304,7 +319,7 @@ def visualize_combined(list_main, list2, list3, df2_ratio, df3_ratio, out, prefi
     ax3.grid(False)
     ax3.set_ylim(0, max(dat2))
     ax3.set_ylabel(ylabel2, font3, labelpad = 11)
-    box = ax3.boxplot(feature2s, positions = xs, notch = False, patch_artist=True)
+    box = ax3.boxplot(feature2s, positions = x_coords, notch = False, patch_artist=True)
     set_boxAttri(box, colors)
 
     ax3.set_xlim(1, xlim)
@@ -313,7 +328,7 @@ def visualize_combined(list_main, list2, list3, df2_ratio, df3_ratio, out, prefi
 
     y_arrow = max(dat2)
     for i in range(0, len(bins)-1): 
-        ax3.arrow(xs[i], y_arrow, 0, -y_arrow/20, overhang=y_arrow/20, head_width=0.2, head_length=1, width = 1, shape="full",fc=colors[i], ec=colors[i],alpha=0.9)
+        ax3.arrow(x_coords[i], y_arrow, 0, -y_arrow/20, overhang=y_arrow/20, head_width=0.2, head_length=1, width = 1, shape="full",fc=colors[i], ec=colors[i],alpha=0.9)
 
 
     ##################################
@@ -325,7 +340,7 @@ def visualize_combined(list_main, list2, list3, df2_ratio, df3_ratio, out, prefi
     ax4.grid(False)
     ax4.set_ylim(0, max(dat3))
     ax4.set_ylabel(ylabel3, font3, labelpad = 11)
-    box2 = ax4.boxplot(feature3s, positions = xs, notch = False, patch_artist=True)
+    box2 = ax4.boxplot(feature3s, positions = x_coords, notch = False, patch_artist=True)
     set_boxAttri(box2, colors)
 
     y_arrow = max(dat3)
@@ -335,7 +350,7 @@ def visualize_combined(list_main, list2, list3, df2_ratio, df3_ratio, out, prefi
     #ax4.set_xlabel(xlabel, font2, labelpad = 10)
     ax4.vlines(tick_pos, 0, max(dat3), color = 'grey', linewidth=ticklinewidth)
     for i in range(0, len(bins)-1): 
-        ax4.arrow(xs[i], y_arrow, 0, -y_arrow/20, overhang=y_arrow/20, head_width=0.2, head_length=1, width = 1, shape="full",fc=colors[i], ec=colors[i], alpha=0.9)
+        ax4.arrow(x_coords[i], y_arrow, 0, -y_arrow/20, overhang=y_arrow/20, head_width=0.2, head_length=1, width = 1, shape="full",fc=colors[i], ec=colors[i], alpha=0.9)
 
 
     
@@ -386,12 +401,17 @@ def visualize_combined(list_main, list2, list3, df2_ratio, df3_ratio, out, prefi
 def main():
     args = getoptions()
     dat = pd.read_csv(args.input_g)
+    if not args.filter == None:
+        gene_filter = pd.read_csv(args.filter, header = None, squeeze = True)
+        dat = dat[dat[args.columnG].isin(gene_filter)].reset_index(drop=True)
     num_list_main = calculate_num_from_pairfile(args.columnG, args.columnT, dat)
     del(dat)
     
     dat_e = pd.read_csv(args.input_e)
+    if not args.filter == None:
+        dat_e = dat_e[dat_e[args.columnG].isin(gene_filter)].reset_index(drop=True)
     dat_e_var = dat_e[dat_e[args.columnR] != 'Constitutive'].reset_index().drop('index', axis=1)
-    #dat_e = remove_duprow(args.columnE, dat_e)
+
     num_list_e = calculate_number(args.columnG, args.columnE, dat_e)
     num_list_e_varXcript = calculate_number(args.columnG, args.columnE, dat_e_var)
     len_list_e = calculate_length(dat_e, args.columnE.replace("_id", ""))
@@ -399,8 +419,10 @@ def main():
     df_ratio_e = calculate_varFeatureRatio(num_list_e_varXcript, num_list_e)
     
     dat_f = pd.read_csv(args.input_f)
+    if not args.filter == None:
+        dat_f = dat_f[dat_f[args.columnG].isin(gene_filter)].reset_index(drop=True)
     dat_f_var = dat_f[dat_f[args.columnR] != 'Constitutive'].reset_index().drop('index', axis=1)
-    #dat_f = remove_duprow(args.columnF, dat_f)
+    
     num_list_f = calculate_number(args.columnG, args.columnF, dat_f)
     num_list_f_varXcript = calculate_number(args.columnG, args.columnF, dat_f_var)
     len_list_f = calculate_length(dat_f, args.columnF.replace("_id", ""))
@@ -428,9 +450,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
